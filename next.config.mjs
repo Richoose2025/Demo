46d3c28e27122b8c588c04424a78c775116c0dbc
Adarsh-oo7 <adarshsarachandran@gmail.com>
2025-03-26 00:26:34 +0530

    ggg

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "/Demo", // Set default for GitHub Pages
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}/` : "", // Ensure assets load correctly
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   }
-}
+};
 
-export default nextConfig
+export default nextConfig;
